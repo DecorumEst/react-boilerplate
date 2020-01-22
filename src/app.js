5b14947cabaae6e5abb1af089aa3f3eb4719f1b1
Antonio Marcos Castelo <marcos.castelo@bpf.com.br>
2020-01-22 18:13:54 -0300

    teste husky

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 'use strict';
-import { hot  } from 'react-hot-loader';
+import { hot } from 'react-hot-loader';
 import React from 'react';
+
 
 function Title () {
   return <h1>Hello World!!!</h1>;
 }
+
 
 export default hot(module)(Title);
